189d98eadb5f974666937c2a91549f4427f34ec5
zengjin <523917455@qq.com>
2018-09-17 15:17:45 +0800

    图书馆信息修改功能

--- home/models.py
+++ home/models.py
@@ -4,3 +4,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Library(models.Model):
+    name = models.CharField(max_length=20)
+    curator = models.CharField(max_length=20)
+    tel = models.IntegerField(max_length=30)
+    address = models.CharField(max_length=30)
+    email = models.CharField(max_length=20)
+    url = models.CharField(max_length=20)
+    create_date = models.CharField(max_length=20)
+    introduce = models.TextField()
+
+    class Meta:
+        db_table = 't_library'
--- home/views.py
+++ home/views.py
@@ -3,24 +3,50 @@ from __future__ import unicode_literals
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
-#系统设置
+# 系统设置
 def modify_view(request):
-    return render(request,'modify.html')
+    if request.method == 'GET':
+        con = Library.objects.first()
+        return render(request, 'modify.html', {'con': con})
+    else:
+        print '1111'
+        name = request.POST.get("libraryname", "")
+        curator = request.POST.get('curator', '')
+        tel = request.POST.get("tel", "")
+        address = request.POST.get("address", "")
+        email = request.POST.get("email", "")
+        url = request.POST.get("url", "")
+        createDate = request.POST.get("createDate", "")
+        introduce = request.POST.get("introduce", "")
+        if Library.objects.first():
+            Library.objects.update(name=name, curator=curator, tel=tel, address=address, email=email, url=url,
+                                   create_date=createDate, introduce=introduce)
+        else:
+            Library.objects.create(name=name, curator=curator, tel=tel, address=address, email=email, url=url,
+                                   create_date=createDate,introduce=introduce)
+        print '执行到这'
+        con = Library.objects.first()
+        return render(request, 'modify.html',{"con":con})
 
 
 def index_view(request):
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
-#管理员设置
+
+# 管理员设置
 def manager_view(request):
-    return render(request,'manager.html')
+    return render(request, 'manager.html')
 
-#参数设置
+
+# 参数设置
 def parameter_view(request):
-    return render(request,'parameter_c.html')
+    return render(request, 'parameter_c.html')
 
-#书架设置
+
+# 书架设置
 def bookcase_view(request):
-    return render(request,'')
+    return render(request, 'bookcase.html')
